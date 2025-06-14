44802c729f28a31ff733ecbc12a830a96b44067b
uhkrg <uhkrg@student.kit.edu>
2025-06-14 17:31:33 +0200

    implement not minimalized ssa

---
+++ src/ssa/initial.rs
@@ -0,0 +1,88 @@
+use std::collections::HashMap;
+
+use crate::{
+    ir::block_ir::{BasicBlock, BlockIR, Statement, Value},
+    parser::Ident,
+    ssa::liveness::calc_liveness,
+};
+
+#[derive(Debug)]
+pub struct Block {
+    parameters: HashMap<Ident, Ident>,
+    block: BasicBlock,
+    end: HashMap<Ident, Ident>,
+}
+
+fn calc_name(v: &Ident, map: &HashMap<Ident, u64>) -> Ident {
+    format!("{}_{v}", map.get(v).unwrap())
+}
+
+pub fn ssaify(ir: BlockIR) -> Vec<Block> {
+    let mut version_map = HashMap::new();
+    let liveness = calc_liveness(&ir);
+    ir.into_iter()
+        .enumerate()
+        .map(|(idx, mut block)| {
+            let parameters = liveness[idx]
+                .iter()
+                .map(|v| (v.to_owned(), next_var_ver(v, &mut version_map)))
+                .collect();
+            ssaify_basic_block(&mut block, &mut version_map);
+            let end = version_map
+                .keys()
+                .cloned()
+                .map(|k| {
+                    let new = calc_name(&k, &version_map);
+                    (k, new)
+                })
+                .collect();
+            Block {
+                parameters,
+                block,
+                end,
+            }
+        })
+        .collect()
+}
+
+fn transform_val(val: &Value, version_map: &mut HashMap<Ident, u64>) -> Value {
+    match val {
+        Value::True => Value::True,
+        Value::False => Value::False,
+        Value::IntConst(c) => Value::IntConst(*c),
+        Value::Var(v) => Value::Var(calc_name(v, version_map)),
+    }
+}
+
+fn next_var_ver(var: &Ident, version_map: &mut HashMap<Ident, u64>) -> Ident {
+    version_map
+        .entry(var.to_owned())
+        .and_modify(|ver| *ver += 1)
+        .or_insert(0);
+    calc_name(var, version_map)
+}
+
+fn ssaify_basic_block(block: &mut BasicBlock, version_map: &mut HashMap<Ident, u64>) {
+    for stmt in block {
+        *stmt = match stmt {
+            Statement::Assign(var, val) => {
+                let val = transform_val(val, version_map);
+                Statement::Assign(next_var_ver(var, version_map), val)
+            }
+            Statement::AssignBinop(var, val1, binop, val2) => {
+                let val1 = transform_val(val1, version_map);
+                let val2 = transform_val(val2, version_map);
+                Statement::AssignBinop(next_var_ver(var, version_map), val1, binop.to_owned(), val2)
+            }
+            Statement::AssignUnop(var, unop, val) => {
+                let val = transform_val(val, version_map);
+                Statement::AssignUnop(next_var_ver(var, version_map), unop.to_owned(), val)
+            }
+            Statement::CondJump(val, target) => {
+                Statement::CondJump(transform_val(val, version_map), *target)
+            }
+            Statement::Jump(target) => Statement::Jump(*target),
+            Statement::Return(val) => Statement::Return(transform_val(val, version_map)),
+        };
+    }
+}
---
+++ src/ssa/unreachable.rs
@@ -0,0 +1,47 @@
+use std::collections::VecDeque;
+
+use crate::ir::block_ir::{BlockIR, Statement};
+
+pub fn remove_unreachable(ir: BlockIR) -> BlockIR {
+    let mut reachable = vec![false; ir.len()];
+    let mut q = VecDeque::from([0]);
+    while let Some(idx) = q.pop_front() {
+        if reachable[idx] {
+            continue;
+        }
+        reachable[idx] = true;
+        match ir[idx].last() {
+            Some(Statement::Jump(t)) => q.push_back(*t),
+            Some(Statement::CondJump(_, t)) => {
+                q.push_back(*t);
+                q.push_back(idx + 1);
+            }
+            Some(Statement::Return(_)) => (),
+            _ => q.push_back(idx + 1),
+        }
+    }
+    let offset = reachable
+        .iter()
+        .scan(0, |so_far, reachable| {
+            if !*reachable {
+                *so_far += 1
+            }
+            Some(*so_far)
+        })
+        .collect::<Vec<_>>();
+    let mut ir = ir
+        .into_iter()
+        .enumerate()
+        .filter(|(idx, _)| reachable[*idx])
+        .map(|(_, v)| v)
+        .collect::<Vec<_>>();
+    for block in &mut ir {
+        let stmt = block.last_mut().unwrap();
+        if let Statement::CondJump(v, target) = stmt {
+            *stmt = Statement::CondJump(v.to_owned(), *target - offset[*target]);
+        } else if let Statement::Jump(target) = stmt {
+            *stmt = Statement::Jump(*target - offset[*target]);
+        }
+    }
+    ir
+}
